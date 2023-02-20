987f3e5b5b77490d0dd83bdd8a1a8b16100b4f1d
IvanIsak2000 <vingving648@gmail.com>
2023-02-20 11:19:02 +0300

    added function

--- blockchain_app.py
+++ blockchain_app.py
@@ -1,30 +1,55 @@
 import hashlib
 import sys
 import os
+import datetime
+import time 
+from tabulate import tabulate
+from tqdm import trange
 
 
 
-def create_block(name, amount, to_whom):#функция для считывания хэша предыдущего блока и создание нового блока 
+
+def get_hash_pre_block():
+
     block_list_directory = os.curdir + '/block_list'#директория папки для хранения всех блоков 
 
     blocks = os.listdir(block_list_directory)
 
-    #определяем максимальный номер блока в папке
     for number in blocks:
         the_number_of_the_most_recent_block = max(number[0])
 
-    new_number_of_block = int(the_number_of_the_most_recent_block) + 1#прибавляем единицу к последнему числу блока
+    with open(f'block_list/{the_number_of_the_most_recent_block}.txt', 'rb') as pre_block:
+        pre_block = pre_block.read()#читаем блок в режиме чтения в двоичном коде
+        pre_hash_of_block = hashlib.md5(pre_block).hexdigest()#получаем хэш ВСЕГО предыдущего блока
+
+    new_number_of_block = int(the_number_of_the_most_recent_block) + 1
+
+    return pre_hash_of_block,new_number_of_block
+
+
+def create_block(name, amount, to_whom):#функция для считывания хэша предыдущего блока и создание нового блока 
+
+    pre_hash_and_pre_number = get_hash_pre_block()
+    pre_hash = pre_hash_and_pre_number[0]
+    new_number_of_block = pre_hash_and_pre_number[1]
+
 
     with open(f'block_list/{new_number_of_block}.txt', 'w') as new_block:#начинаем создание нового блока
+        os.system('cls')
+        print("Creating block!")
+        for i in trange(1000):
+            time.sleep(0.01)
 
-        with open(f'block_list/{the_number_of_the_most_recent_block}.txt', 'rb') as pre_block:
-            pre_block = pre_block.read()#читаем блок в режиме чтения в двоичном коде
-            pre_hash = hashlib.md5(pre_block).hexdigest()#получаем хэш ВСЕГО предыдущего блока
-            print(f'pre hash: {pre_hash}')#выводим хэш прошлого блока
+        current_date_time = datetime.datetime.now()
+        current_time = current_date_time.time()
 
-        new_block_text = f'from:{name}\namount:{amount}\nto whom:{to_whom}\npre_hash{pre_hash}'#создаём текст нового блока:имя,цену,кому, и записываем хэш прошлого блока
+        new_block_text = f'from:{name}\namount:{amount}\nto whom:{to_whom}\ntime:{current_time}\npre_hash{pre_hash}'#создаём текст нового блока:имя,цену,кому, и записываем хэш прошлого блока
         new_block.write(new_block_text)#записываем в новый блок
-        print('All done!')
+    os.system('cls')
+    print('All done!')
+
+    table = [['From',name],['Amount',amount],['To whom',to_whom],['Time',current_time],['Status','successful']]
+    print(tabulate(table))
 
 
 if __name__ == '__main__':

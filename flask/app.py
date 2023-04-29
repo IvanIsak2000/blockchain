c947635cc84075b3780a08702e9b6302d1e83091
VANECK <vingving648@gmail.com>
2023-04-29 14:59:18 +0300

    add main flask app

---
+++ flask/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import  request,render_template
+app = Flask(__name__, template_folder='templates') 
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/signup/" )
+def signup():
+    print('чел вошёл в акк')
+    return render_template("signup.html")
+    
+
+@app.route("/signin/" )
+def signin():
+    return render_template("signin.html")
+
+
+
+
+
+
+
+app.run(host="0.0.0.0",port=8000,debug=True)

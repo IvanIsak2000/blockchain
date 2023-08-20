2aefc103e67ab8a7613aa18bae7161be91147f46
mb6ockatf <mdddmmmm@yandex.ru>
2023-08-20 17:24:58 +0300

    style: update ui with bootstrap

--- website/app.py
+++ website/app.py
@@ -1,17 +1,18 @@
-from flask import Flask
-from flask import  request,render_template
-app = Flask(__name__, template_folder='templates') 
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/signup/" )
+@app.route("/signup")
 def signup():
-    return render_template("signup.html") 
-@app.route("/signin/" )
+    return render_template("signup.html")
+
+@app.route("/signin")
 def signin():
     return render_template("signin.html")
 
 
-app.run(host="0.0.0.0",port=8000,debug=True)
+app.run(host="0.0.0.0", port=8000, debug=True)
